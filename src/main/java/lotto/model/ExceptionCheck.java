40f9fe9a7c8a79bda10e030f0c05b5d1b7baa472
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-11 18:10:45 +0900

    feat : 추가 당첨번호 예외 처리 기능

--- src/main/java/lotto/model/ExceptionCheck.java
+++ src/main/java/lotto/model/ExceptionCheck.java
@@ -1,5 +1,9 @@
 package lotto.model;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ExceptionCheck {
     public static void inputMoneyFormatCheck(String input) throws IllegalArgumentException{
         if(input.replaceAll("[^0-9]","").length()!=input.length()){
@@ -7,6 +11,27 @@ public class ExceptionCheck {
         }
         if(Integer.parseInt(input)%1000 != 0){
             throw new IllegalArgumentException(ExceptionMessage.ERROR_WRONG_NUMBER.getMessage());
+        }
+    }
+    public static void inputAnswerNumberCheck(String input) throws IllegalArgumentException{
+        answerNumberFormatCheck(input);
+        String[] numbers = input.split(",");
+        answerNumberLengthCheck(numbers);
+        answerNumberOverlapCheck(numbers);
+    }
+    private static void answerNumberFormatCheck(String input) throws IllegalArgumentException{
+        if(input.replaceAll("[^0-9\\,]","").length() != input.length()){
+            throw new IllegalArgumentException(ExceptionMessage.ERROR_NOT_ANSWER.getMessage());
+        }
+    }
+    private static void answerNumberLengthCheck(String[] numbers) throws IllegalArgumentException{
+        if(numbers.length != 6){
+            throw new IllegalArgumentException(ExceptionMessage.ERROR_WRONG_LENGTH.getMessage());
+        }
+    }
+    private static void answerNumberOverlapCheck(String[] numbers) throws IllegalArgumentException{
+        if(Arrays.stream(numbers).distinct().count() != numbers.length){
+            throw new IllegalArgumentException(ExceptionMessage.ERROR_OVERLAP.getMessage());
         }
     }
 }
--- src/main/java/lotto/model/ExceptionMessage.java
+++ src/main/java/lotto/model/ExceptionMessage.java
@@ -1,12 +1,17 @@
 package lotto.model;
 
 public enum ExceptionMessage {
-    ERROR_NOT_NUMBER("숫자를 입력해주세요"), ERROR_WRONG_NUMBER("1000의 배수를 입력해 주세요");
+    ERROR_NOT_NUMBER("숫자를 입력해주세요"), ERROR_WRONG_NUMBER("1000의 배수를 입력해 주세요")
+    ,ERROR_NOT_ANSWER("숫자와 , 를 이용하여 입력해주세요"), ERROR_WRONG_LENGTH("숫자 6개를 입력해주세요")
+    ,ERROR_OVERLAP("중복된 숫자가 있습니다");
+
     static final String HEADER = "[ERROR] ";
     String message;
+
     ExceptionMessage(String body){
         this.message = HEADER + body;
     }
+
     public String getMessage(){
         return message;
     }
