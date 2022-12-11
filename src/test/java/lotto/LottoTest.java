34b029971dcc16cc6ee0527a1171444588d04ced
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-11 17:26:33 +0900

    test : 추가 Lotto객체 내 숫자열을 String으로 리턴하는 기능 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -22,6 +23,10 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
+    @DisplayName("로또 내 번호들을 출력한다")
+    @Test
+    void numbersToString(){
+        assertThat(new Lotto(List.of(1,2,3,4,5,6)).toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
+    }
     // 아래에 추가 테스트 작성 가능
 }
