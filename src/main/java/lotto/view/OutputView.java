928d0d2e0e4bff3aeafd36e4a8e09760e4db9c5c
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-11 17:25:45 +0900

    feat : 추가 Lotto 구매 확인 메시지와 숫자들  출력 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,4 +4,10 @@ public class OutputView {
     public static void printPurchaseMessage() {
         System.out.println("구입금액을 입력해 주세요.");
     }
+    public static void printSuccessPurchase(int number){
+        System.out.println(number+"개를 구매했습니다");
+    }
+    public static void printDetailNumbers(String numbers){
+        System.out.println(numbers);
+    }
 }
