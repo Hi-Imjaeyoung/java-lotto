922fe363376867a09d92cda9cbec86f2f2e769f9
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-11 17:11:10 +0900

    refactor : 수정 클래스 이름 오타
    Numer -> Number로 수정

--- src/main/java/lotto/model/RandomNumberMaker.java
+++ src/main/java/lotto/model/RandomNumberMaker.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class RandomNumerMaker {
+public class RandomNumberMaker {
     public List<Integer> RandomNumberMaker(){
         List<Integer> numbers = Collections.emptyList();
         while(true){
