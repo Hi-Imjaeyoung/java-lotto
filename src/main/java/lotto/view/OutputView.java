92e6fa2e6f862477e90b2cfe830621583ba1bdf9
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-12 22:24:06 +0900

    feat : 추가 결과메시지 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -16,4 +16,15 @@ public class OutputView {
     public static void printBonusNumberRequest(){
         System.out.println("보너스 번호를 입력해 주세요.");
     }
+    public static void printResult(String rankings , String profit){
+        StringBuilder resultLog = new StringBuilder();
+        resultLog.append("당첨 통계"+"\n"+"---"+"\n");
+        resultLog.append("3개 일치 (5,000원) - "+rankings.charAt(4)+"개"+"\n");
+        resultLog.append("4개 일치 (50,000원) - "+rankings.charAt(3)+"개"+"\n");
+        resultLog.append("5개 일치 (1,500,000원) - "+rankings.charAt(2)+"개"+"\n");
+        resultLog.append("5개 일치, 보너스 볼 일치 (30,000,000원) - "+rankings.charAt(1)+"개"+"\n");
+        resultLog.append("6개 일치 (2,000,000,000원) - "+rankings.charAt(0)+"개"+"\n");
+        resultLog.append("총 수익률은 "+profit+"입니다.");
+        System.out.println(resultLog);
+    }
 }
