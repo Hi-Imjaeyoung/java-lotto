a94c97bf37898ed1a0f8f4abb28c518c4d44f060
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-12 22:15:55 +0900

    feat : 추가 특정 String 을 가지는 Enum을 찾아 리턴하는 기능추가.

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,8 +1,18 @@
 package lotto.model;
 
+import java.awt.geom.NoninvertibleTransformException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.Map;
+
 public enum Rank {
-    FIRST("1",2_000_000_000), SECOND("2",30_000_000), THIRD("3",1_500_00),FOUTH("4",50_000),
-    FIFTH("5",5_00), FAIL("-1",0);
+    FIRST("1",2_000_000_000), SECOND("2",30_000_000),
+    THIRD("3",1_500_00),FOURTH("4",50_000),
+    FIFTH("5",5_000), FAIL("-1",0);
+
     String ranking;
     int money;
     Rank(String ranking, int money){
@@ -14,5 +24,12 @@ public enum Rank {
     }
     public int getMoney(){
         return money;
+    }
+
+    public static Rank findRanking(String rank){
+        return Arrays.stream(Rank.values())
+                .filter(ranking -> ranking.ranking.equals(rank))
+                .findAny()
+                .orElse(FAIL);
     }
 }
