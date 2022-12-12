183de4f8d23556dffab5337c4d46c0524ad3b762
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-12 22:17:49 +0900

    test : 추가 RankingCalculate 테스트 코드 작성

---
+++ src/test/java/lotto/model/RankingCalculateTest.java
@@ -0,0 +1,37 @@
+package lotto.model;
+
+import lotto.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RankingCalculateTest {
+    @DisplayName("lotto의 결과 계산")
+    @Test
+    void lottoResultCalculate(){
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(List.of(1,2,3,4,7,8))); //1등
+        lottos.add(new Lotto(List.of(1,2,3,4,7,6))); //1등
+        lottos.add(new Lotto(List.of(1,2,3,4,5,6))); //2등
+        lottos.add(new Lotto(List.of(1,2,3,4,7,10))); //3등
+        lottos.add(new Lotto(List.of(1,2,3,4,10,11))); //4등
+        lottos.add(new Lotto(List.of(1,2,3,9,10,11))); //5등
+
+        List<Integer> numbers = List.of(1,2,3,4,7,8);
+        int bonusNumber = 6;
+        RankingCalculate rankingCalculate = new RankingCalculate(lottos,numbers,bonusNumber);
+        assertThat(rankingCalculate.getRankings()).isEqualTo("21111");
+    }
+    @Test
+    void lottoProfitCalculate(){
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(List.of(1,2,3,9,10,11))); //5등
+        List<Integer> numbers = List.of(1,2,3,4,7,8);
+        int bonusNumber = 6;
+        RankingCalculate rankingCalculate = new RankingCalculate(lottos,numbers,bonusNumber);
+        assertThat(rankingCalculate.getProfit()).isEqualTo("500.0");
+    }
+}
