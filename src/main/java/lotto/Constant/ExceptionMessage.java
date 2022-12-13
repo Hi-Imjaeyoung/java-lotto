2ab0420de5dc2c68c7d9ee81f89ba9a56adf5d88
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-13 15:11:54 +0900

    refactor : 안쓰는 enum 제거

--- src/main/java/lotto/Constant/ExceptionMessage.java
+++ src/main/java/lotto/Constant/ExceptionMessage.java
@@ -10,21 +10,10 @@ public enum ExceptionMessage {
     ,ERROR_OVERLAP("중복된 숫자가 있습니다"),ERROR_SCALE("1부터 45 사이의 숫자를 입력해주세요");
 
     static final String HEADER = "[ERROR] ";
-    int code;
-    List<Objects> list =Collections.emptyList();
     String message;
-    ExceptionMessage(int code, List<Objects> list){
-        this.code = code;
-        this.list = list;
-    }
+
     ExceptionMessage(String body){
         this.message = HEADER + body;
-    }
-    public int getCode(){
-        return code;
-    }
-    public List<Objects> getList(){
-        return list;
     }
     public String getMessage(){
         return message;
