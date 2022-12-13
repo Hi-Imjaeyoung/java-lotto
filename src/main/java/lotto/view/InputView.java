6f1dee0be44ffc84846f19ec3dc0b7bc748a7477
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-13 12:56:20 +0900

    refactor : InputView의 의미를 확장
    출력 > 아웃 , 입력 > 인 이라고 정해놓고 하니 cotroller에서 코드가 복잡해짐
    이를 해결하기 위해, 기능 별로 다시 묶음

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,15 +3,19 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+
     public static String inputMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
         String money = Console.readLine();
         return money;
     }
     public static String inputAnswerNumbers(){
+        System.out.println("당첨 번호를 입력해 주세요");
         String answerNumbers = Console.readLine();
         return answerNumbers;
     }
     public static String inputBonusNumber(){
+        System.out.println("보너스 번호를 입력해 주세요.");
         String bonusNumber = Console.readLine();
         return bonusNumber;
     }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,20 +1,12 @@
 package lotto.view;
 
 public class OutputView {
-    public static void printPurchaseRequest() {
-        System.out.println("구입금액을 입력해 주세요.");
-    }
+
     public static void printSuccessPurchase(int number){
         System.out.println(number+"개를 구매했습니다.");
     }
     public static void printDetailNumbers(String numbers){
         System.out.println(numbers);
-    }
-    public static void printAnswerNumberRequest(){
-        System.out.println("당첨 번호를 입력해 주세요");
-    }
-    public static void printBonusNumberRequest(){
-        System.out.println("보너스 번호를 입력해 주세요.");
     }
     public static void printResult(String rankings , String profit){
         StringBuilder resultLog = new StringBuilder();
