0c3a7392ebbdfd28e67a4b273f5e8c5f896649ec
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-13 12:51:05 +0900

    refactor : main에서 예외를 처리를 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.model.ExceptionCheck;
+import lotto.model.ExceptionMessage;
 import lotto.model.RandomNumberMaker;
 import lotto.model.RankingCalculate;
 import lotto.view.InputView;
@@ -8,31 +9,23 @@ import lotto.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Application { ;
     static private RandomNumberMaker randomNumberMaker =new RandomNumberMaker();
     public static void main(String[] args) {
-        int inputMoney;
-        try {
-            inputMoney = checkInputMoney();
-        }catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
+        int inputMoney = checkInputMoney();
+        if(inputMoney == ExceptionMessage.ERROR_CODE.getCode()){
             return;
         }
         List<Lotto> lottos = makeLottos(inputMoney);
-        for(Lotto lotto :lottos){
-            OutputView.printDetailNumbers(lotto.toString());
-        }
+        numbersPrint(lottos);
         OutputView.printSuccessPurchase(inputMoney/1000);
-        List<Integer> answerNumbers;
-        int bonusNumber;
-        try{
-            answerNumbers = checkAnswerNumber();
-            bonusNumber = checkBonusNumber(answerNumbers);
-        }catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
+        List<Integer> answerNumbers = checkAnswerNumber();
+        int bonusNumber = checkBonusNumber(answerNumbers);
+        if(answerNumbers.isEmpty() || bonusNumber == ExceptionMessage.ERROR_CODE.getCode()){
             return;
         }
         RankingCalculate rankingCalculate = new RankingCalculate(lottos,answerNumbers,bonusNumber);
@@ -41,7 +34,12 @@ public class Application { ;
     private static int checkInputMoney() throws IllegalArgumentException{
         OutputView.printPurchaseRequest();
         String inputMoney = InputView.inputMoney();
-        ExceptionCheck.inputMoneyFormatCheck(inputMoney);
+        try {
+            ExceptionCheck.inputMoneyFormatCheck(inputMoney);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return -1;
+        }
         return Integer.parseInt(inputMoney);
     }
     private static List<Lotto> makeLottos(int inputMoney){
@@ -54,7 +52,12 @@ public class Application { ;
     private static List<Integer> checkAnswerNumber(){
         OutputView.printAnswerNumberRequest();
         String answerNumber = InputView.inputAnswerNumbers();
-        ExceptionCheck.inputAnswerNumberCheck(answerNumber);
+        try {
+            ExceptionCheck.inputAnswerNumberCheck(answerNumber);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return Collections.emptyList();
+        }
         return Arrays.stream(answerNumber.split(","))
                 .map(value -> Integer.parseInt(value))
                 .collect(Collectors.toList());
@@ -62,7 +65,17 @@ public class Application { ;
     private static int checkBonusNumber(List<Integer> answerNumbers){
         OutputView.printBonusNumberRequest();
         String bonusNumber = InputView.inputBonusNumber();
-        ExceptionCheck.bonusNumberCheck(bonusNumber,answerNumbers);
+        try {
+            ExceptionCheck.bonusNumberCheck(bonusNumber,answerNumbers);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return -1;
+        }
         return Integer.parseInt(bonusNumber);
+    }
+    private static void numbersPrint(List<Lotto> lottos){
+        for(Lotto lotto :lottos){
+            OutputView.printDetailNumbers(lotto.toString());
+        }
     }
 }
